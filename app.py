607b437d0204727900b9d44cc6774b410ce6d4c9
James Hou <houjames05@gmail.com>
2020-04-30 22:57:30 -0700

    bad stuff

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 def readDatabase(root):
+    """
+    Purpose: This function reads all the
+    Parameters:
+    Returns:
+    """
     file = open(root, 'r')
     tests = []
+    FIRSTTIME = False
     for line in file:
-        if "example test" in line == False:
+        if "first-time" in line:
+              FIRSTTIME = True
+        else:
             line = line.strip().split(',')
             tests.append(line)
-    file.close()
-    return tests
 
+    file.close()
+    return tests, FIRSTTIME
+
+def updateDatabase(root, tests):
+    file = open(root, 'w')
+    for i in range(len(tests)):
+        file.write(','.join(tests[i]) + "\n")
+
+    file.close()
+
+def displayWelcome(FIRSTTIME, tests):
+    if FIRSTTIME:
+        print("Welcome to the Study App.")
+        print("This app will help you study for any test.")
+        print("This app compiles all the resources online and displays them to you in an organized way.")
+        print("Here are your ")
 
 if __name__ == "__main__":
-    l = readDatabase('database.txt')
-    print(l)
+    APPISACTIVE = True
+    database = "database.txt"
+    tests, FIRSTTIME = readDatabase(database)
+    displayWelcome(FIRSTTIME, tests)
+    if FIRSTTIME:
+        updateDatabase(database, tests)
+        FIRSTTIME = False
+
+    CHANGEISMADE = False
